13b3f6a119a94af459419090fb85aef67272b62c
Rafael Santos <tucif.x@gmail.com>
2010-02-12 15:10:32 -0600

    subido main.py

---
+++ main.py
@@ -0,0 +1,148 @@
+# coding: utf-8
+
+import sys
+import gtk
+import cairo
+import gobject
+import pygtk
+import random
+pygtk.require('2.0')
+#from datetime import datetime
+
+#Lienzo es donde se pintara todo
+class Lienzo(gtk.DrawingArea):
+    def __init__(self, ventana):
+        super(Lienzo, self).__init__()
+
+        #Cambiar el color de fondo de la ventana
+        self.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(0,0,0))
+        # Pedir el tamano de la ventana
+        self.set_size_request(400,400)
+
+        #Asignar la ventana que recibe de paramentro a la ventana que se
+        #utilizara en el lienzo
+        self.ventana=ventana
+
+        #expose-event es una propiedad de DrawingArea que le dice como
+        #dibujares, aqui le decimos que utilize nuestra funcion paint
+        #para ese evento en vez del que trae por defaul.
+        self.connect("expose-event", self.paint)
+        #reconocer cuando oprimes y sueltas el mouse
+        self.connect("button_press_event",self.button_press)
+        self.connect("button_release_event",self.button_release)
+        self.connect("motion_notify_event",self.actualizarDragged)
+        self.set_events(gtk.gdk.BUTTON_PRESS_MASK|gtk.gdk.BUTTON_RELEASE_MASK|gtk.gdk.POINTER_MOTION_MASK)
+
+        #Inicializar todos los valores
+        self.init_simulation()
+
+       
+        self.draggingObject = None
+        self.corriendo = True
+
+        self.objetoSeleccionado=[]
+
+
+    def actualizarDragged(self,widget,event):
+        if self.draggingObject:
+            self.draggingObject.posX=event.x
+            self.draggingObject.posY=event.y
+
+    def on_timer(self):
+        self.update()
+        return True
+
+    def init_simulation(self):
+        """Inicializacion de valores"""
+        gobject.timeout_add(20, self.on_timer)
+
+
+
+    def update(self):
+        
+        self.queue_draw()
+
+    def paint(self, widget, event):
+        """Nuestro metodo de pintado propio"""
+
+        #Se crea un widget de cairo que despues se usara para desplegar
+        #todo en la ventana
+        cr = widget.window.cairo_create()
+        #Le decimos a cairo que pinte su widget por primera vez.
+        cr.set_source_rgb(0,0,0)
+        cr.paint()
+
+        #pintar a los agentes
+        
+
+        
+
+        
+
+        #pintar efecto de selecci�n sobre un agente
+        if self.objetoSeleccionado:
+            cr.set_line_width(2)
+            cr.set_source_rgba(random.random(), 1, random.random(), 0.3)
+            cr.rectangle(self.objetoSeleccionado.posX-20,self.objetoSeleccionado.posY-20,
+                            self.objetoSeleccionado.ancho+40, self.objetoSeleccionado.alto+40)
+
+            cr.stroke()
+
+        #pintar la informaci�n del agente seleccionado
+        
+
+    #Para drag & drop
+    def button_press(self,widget,event):
+        if event.button == 1:
+            pass
+                
+    def button_release(self,widget,event):
+        pass
+
+    def pausar(self):
+        self.corriendo=False
+
+    def correr(self):
+        self.corriendo=True
+
+
+
+class Main(gtk.Window):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.faseActual = 4
+        self.set_title('Proyecto')
+        self.set_size_request(400,400)
+        self.set_resizable(False)
+
+        #mainBox contiene el menu superior, contentBox(menu,lienzo) y el menu inferior
+        self.mainBox = gtk.VBox(False,0)
+        self.mainBox.set_size_request(400,400)
+
+        
+        #contentBox contiene el menu lateral y lienzo
+        self.contentBox= gtk.HBox(False,0) #Recibe False para no se homogeneo
+        
+        self.lienzo=Lienzo(self)
+        self.lienzo.set_size_request(400,400)
+
+        self.contentBox.pack_start(self.lienzo, expand=False, fill=True, padding=0)
+        
+        #Empaquetado de todos los controles
+        
+        self.mainBox.pack_start(self.contentBox,expand=False, fill=True, padding=0)
+        #Agregar la caja que contiene todo a la ventana
+        self.add(self.mainBox)
+        self.connect("destroy", gtk.main_quit)
+        self.show_all()
+
+    def pausarLienzo(self, widget):
+        self.lienzo.pausar()
+
+    def correrLienzo(self, widget):
+        self.lienzo.correr()
+
+    
+
+Main()
+gtk.main()
