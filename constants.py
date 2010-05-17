9e63b31d984018c6eee903a97c6329b41a4e5e3f
Rafael Santos <tucif.x@gmail.com>
2010-05-16 22:52:53 -0500

    Fix for packaging

--- constants.py
+++ constants.py
@@ -2,10 +2,10 @@ import gtk
 
 WINDOW_SIZE=700
 
-TOTAL_VIRUS = 8
+TOTAL_VIRUS = 6
 TOTAL_ENVIRONMENTS=1
 
 MAX_FITNESS=127*4
 
 #imagenes
-VIRUS_IMAGE=gtk.gdk.pixbuf_new_from_file("resources/virus/virus.png")
+VIRUS_IMAGE=gtk.gdk.pixbuf_new_from_file("./resources/virus/virus.png")
